b0971317db6fa1a743268ac9d38b31c03cb3fb62
AkoIsCat <109052469+AkoIsCat@users.noreply.github.com>
2023-09-04 00:39:42 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -1,20 +1,9 @@
-const [a, b] = require('fs')
+const input = require('fs')
   .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .replace(/\r/g, '')
   .split(' ');
 
-const reverseNumber = (num) => {
-  const firstNum = num[0];
-  const middleNum = num[1];
-  const endNum = num[2];
-  const tempNum = firstNum;
-
-  return +(endNum + middleNum + tempNum);
-};
-
-const reverseOfA = reverseNumber(a);
-const reverseOfB = reverseNumber(b);
-
-console.log(reverseOfA > reverseOfB ? reverseOfA : reverseOfB);
+const [num1, num2] = input.map((item) => +item.split('').reverse().join(''));
+console.log(Math.max(num1, num2));
