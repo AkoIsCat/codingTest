e3a174fd43a3a53e5d0084b8894eae2bc8ad7783
AkoIsCat <109052469+AkoIsCat@users.noreply.github.com>
2023-09-05 21:45:48 +0900

    [Bronze V] Title: 새싹, Time: 128 ms, Memory: 9280 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,12 @@
+const first = `         ,r'"7`;
+const second = "r`-_   ,'  ,/";
+const third = ` \\. ". L_r'`;
+const fourth = '   `~\\/';
+const fifth = '      |';
+
+console.log(`${first}
+${second}
+${third}
+${fourth}
+${fifth}
+${fifth}`);
