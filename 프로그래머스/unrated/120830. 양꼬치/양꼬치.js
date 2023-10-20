f7b31be614163b293f32afd6ef7e6a6ad293a010
김은혜 <109052469+AkoIsCat@users.noreply.github.com>
2023-10-20 19:04:35 +0900

    [level unrated] Title: 양꼬치, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.js
@@ -1,5 +1,5 @@
 function solution(n, k) {
-    const service = parseInt(n / 10);
+    const service = Math.floor(n / 10);
     const meatPrice = n * 12000;
     const drinkPrice = (k - service) * 2000;
     return meatPrice + drinkPrice;
